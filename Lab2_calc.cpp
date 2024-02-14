c5856e9956f6a96c0d6572d55399b2cddaaea098
joshzano <158782774+joshzano@users.noreply.github.com>
2024-02-13 21:16:43 -0800

    Add files via upload

---
+++ Lab2_calc.cpp
@@ -0,0 +1,18 @@
+//Lab2_calc.cpp
+/***
+ * {Will learn different ways to represent numbers to memory}
+ ***/
+
+ #include <iostream>
+ #include <cstdib>
+ using namespace std;
+
+ int main (){
+    // numerical
+    int int_x = 1;
+    int int_y = 2;
+    double double_z = 2.0;
+
+    cout << "int divided by int; " << int_x / int_y << endl;
+    cout << "int divided by double: " << int_x / double_z << end1;
+    }
